3d309131693392548264893c563b81c3ea62fd1b
H52e5 <2737164694@qq.com>
2022-06-13 11:48:43 +0800

    chore: init

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { extname } = require('path');
+
+hexo.config.sitemap = Object.assign({
+  path: ['sitemap.xml', 'sitemap.txt', 'sitemap.html'],
+  rel: false,
+  tags: true,
+  categories: true
+}, hexo.config.sitemap);
+
+const config = hexo.config.sitemap;
+
+if (typeof config.path === 'string' && !extname(config.path)) {
+  config.path += '.xml';
+}
+
+hexo.extend.generator.register('sitemaps', require('./lib/generator'));
+
+if (config.rel === true) {
+  hexo.extend.filter.register('after_render:html', require('./lib/rel'));
+}
